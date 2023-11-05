c08b2f5ba8262a8b05a2e9d0df943eac086e7e05
kgarrels <kai.garrels@me.com>
2023-11-05 15:06:29 +0100

    dockaudio, remember mute status in settings

--- src/qtgui/dockaudio.cpp
+++ src/qtgui/dockaudio.cpp
@@ -327,6 +327,7 @@ void DockAudio::setAudioPlayButtonState(bool checked)
 void DockAudio::saveSettings(QSettings *settings)
 {
     int     ival, fft_min, fft_max;
+    bool    muted;
 
     if (!settings)
         return;
@@ -335,12 +336,16 @@ void DockAudio::saveSettings(QSettings *settings)
 
     settings->setValue("gain", audioGain());
 
+    muted = ui->audioMuteButton->isChecked();
+    settings->setValue("muted", muted);
+    
     ival = audioOptions->getFftSplit();
     if (ival != DEFAULT_FFT_SPLIT)
         settings->setValue("fft_split", ival);
     else
         settings->remove("fft_split");
 
+   
     audioOptions->getPandapterRange(&fft_min, &fft_max);
     if (fft_min != -80)
         settings->setValue("pandapter_min_db", fft_min);
@@ -392,7 +397,7 @@ void DockAudio::saveSettings(QSettings *settings)
 void DockAudio::readSettings(QSettings *settings)
 {
     int     bool_val, ival, fft_min, fft_max;
-    bool    conv_ok = false;
+    bool    conv_ok = false, muted;
 
     if (!settings)
         return;
@@ -403,6 +408,10 @@ void DockAudio::readSettings(QSettings *settings)
     if (conv_ok)
         setAudioGain(ival);
 
+    muted = settings->value("muted", false).toBool();
+    on_audioMuteButton_clicked(muted);
+    ui->audioMuteButton->setChecked(muted);
+    
     ival = settings->value("fft_split", DEFAULT_FFT_SPLIT).toInt(&conv_ok);
     if (conv_ok)
         audioOptions->setFftSplit(ival);
@@ -492,9 +501,9 @@ void DockAudio::muteToggleShortcut() {
 }
 
 void DockAudio::increaseAudioGainShortcut() {
-	ui->audioGainSlider->triggerAction(QSlider::SliderPageStepAdd);
+    ui->audioGainSlider->triggerAction(QSlider::SliderPageStepAdd);
 }
 
 void DockAudio::decreaseAudioGainShortcut() {
-	ui->audioGainSlider->triggerAction(QSlider::SliderPageStepSub);
+    ui->audioGainSlider->triggerAction(QSlider::SliderPageStepSub);
 }
