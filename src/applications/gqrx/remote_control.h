fe031b82b358abae5491fef4b52d2fd2168cabf7
kgarrels <kai.garrels@me.com>
2023-11-05 14:50:12 +0100

    remote, read buffer while byte are available

--- src/applications/gqrx/remote_control.h
+++ src/applications/gqrx/remote_control.h
@@ -92,6 +92,7 @@ public slots:
     void setMode(int mode);
     void setPassband(int passband_lo, int passband_hi);
     void setSquelchLevel(double level);
+    void setNoisefloor(double level);
     void setAudioGain(float gain);
     void startAudioRecorder(QString unused);
     void stopAudioRecorder();
@@ -135,6 +136,7 @@ private:
     bool        rds_status;        /*!< RDS decoder enabled */
     float       signal_level;      /*!< Signal level in dBFS */
     double      squelch_level;     /*!< Squelch level in dBFS */
+    float       noisefloor;        /* noisfloor from plotter */
     float       audio_gain;        /*!< Audio gain in dB */
     QString     rc_program_id;     /*!< RDS Program identification */
     bool        audio_recorder_status; /*!< Recording enabled */
