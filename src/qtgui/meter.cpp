89320b53a237149053bcac6400ca41dee3d70c15
kgarrels <kai.garrels@me.com>
2023-11-07 13:16:54 +0100

    meter, single click sets noisefloor correction to actual signal level

--- src/qtgui/meter.cpp
+++ src/qtgui/meter.cpp
@@ -33,6 +33,9 @@
 #include <cmath>
 #include <QDebug>
 #include "meter.h"
+#include "plotter.h"    // for m_Noisefloor
+
+Q_LOGGING_CATEGORY(meter, "meter")
 
 // ratio to total control width or height
 #define CTRL_MARGIN 0.07        // left/right margin
@@ -44,8 +47,10 @@
 #define MIN_DB -100.0f
 #define MAX_DB +0.0f
 
-#define ALPHA_DECAY     0.25f
-#define ALPHA_RISE      0.70f
+#define ALPHA_DECAY         0.5f
+#define ALPHA_RISE          0.5f
+#define ALPHA_PEAK_DECAY    0.05f   // +kai peak indicator
+#define ALPHA_PEAK_RISE     0.95f
 
 CMeter::CMeter(QWidget *parent) : QFrame(parent)
 {
@@ -70,17 +75,30 @@ QSize CMeter::sizeHint() const
     return QSize(100, 30);
 }
 
-void CMeter::setLevel(float dbfs)
+void CMeter::setLevel(float dbfs, float noisefloor)
 {
     const float old = m_dBFS;
-    float alpha = dbfs < m_dBFS ? ALPHA_DECAY : ALPHA_RISE;
+    
     m_dBFS -= alpha * (m_dBFS - dbfs);
+    if (dbfs < MIN_DB)
+        dbfs = MIN_DB;
+    else if (dbfs > MAX_DB)
+        dbfs = MAX_DB;
+
+    float alpha  = dbfs < m_dBFS ? ALPHA_DECAY : ALPHA_RISE;
+    float alphaPeak  = dbfs < m_dBFSPeak ? ALPHA_PEAK_DECAY : ALPHA_PEAK_RISE;
+
+    m_dBFS -= alpha * (m_dBFS - dbfs);
+    m_dBFSPeak -= alphaPeak * (m_dBFSPeak - dbfs);
+
+    m_Noisefloor = noisefloor;
     // only redraw when the label needs to change
     if (qRound(m_dBFS * 10) != qRound(old * 10))
     {
         update();
     }
 }
+    
 
 void CMeter::setSqlLevel(float dbfs)
 {
@@ -112,7 +130,20 @@ void CMeter::draw(QPainter &painter)
         pen.setJoinStyle(Qt::MiterJoin);
         painter.setPen(pen);
         painter.setBrush(QBrush(color));
+
         painter.drawRect(QRectF(marg, ht + 2, (qreal)(std::min(m_dBFS, MAX_DB) - MIN_DB) * pixperdb, 4));
+
+        // draw peak level
+        qreal x = marg + (qreal)(m_dBFSPeak - MIN_DB) * pixperdb;
+        painter.setPen(QPen(Qt::green, 1, Qt::SolidLine));             
+        painter.drawLine(QLineF(x, hline+2, x, hline+6));
+    }
+
+    if (m_Sql > MIN_DB)
+    {
+        qreal x = marg + (qreal)(m_Sql - MIN_DB) * pixperdb;
+        painter.setPen(QPen(Qt::yellow, 1, Qt::SolidLine));
+        painter.drawLine(QLineF(x, hline, x, hline + 8));
     }
 
     if (m_Sql > MIN_DB)
@@ -126,7 +157,11 @@ void CMeter::draw(QPainter &painter)
     painter.setFont(m_font);
 
     painter.setPen(QColor(0xDA, 0xDA, 0xDA, 0xFF));
-    painter.drawText(marg, height() - 2, QString::number((double)m_dBFS, 'f', 1) + " dBFS" );
+
+    //painter.drawText(marg, height() - 2, QString::number((double)m_dBFS, 'f', 1) + " dBFS" );
+    
+    // calculate SNR by using signalPeak and noisefloor
+    painter.drawText(marg, height() - 2, QString::number(m_dBFSPeak - m_Noisefloor - m_NoisefloorCorrection, 'f', 1) + " dB SN" );
 }
 
 // Called to draw an overlay bitmap containing items that
@@ -163,4 +198,10 @@ void CMeter::drawOverlay(QPainter &painter)
         painter.drawText(rect, Qt::AlignHCenter|Qt::AlignVCenter, QString::number(x));
         rect.translate(rwidth, 0);
     }
+}
+
+void CMeter::mousePressEvent(QMouseEvent *event)
+{
+    m_NoisefloorCorrection = m_dBFSPeak - m_Noisefloor ;
+    qCDebug(meter) << "noisefloor correction: " << m_NoisefloorCorrection;
 }
