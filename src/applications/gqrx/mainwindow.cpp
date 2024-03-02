9465611f1fa7556defe833aa64bc49136d03b187
kgarrels <kai.garrels@me.com>
2024-03-02 11:47:28 +0100

    remote, allow seeting of bookmarks with "bookmark name tag"

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -345,6 +345,7 @@ MainWindow::MainWindow(const QString& cfgfile, bool edit_conf, QWidget *parent) 
     connect(remote, SIGNAL(newPassband(int)), this, SLOT(setPassband(int)));
     connect(remote, SIGNAL(gainChanged(QString, double)), uiDockInputCtl, SLOT(setGain(QString,double)));
     connect(remote, SIGNAL(dspChanged(bool)), this, SLOT(on_actionDSP_triggered(bool)));
+    connect(remote, SIGNAL(bookmarksChanged(bool)), ui->plotter, SLOT(updateOverlay()));
     connect(uiDockRDS, SIGNAL(rdsPI(QString)), remote, SLOT(rdsPI(QString)));
 
     rds_timer = new QTimer(this);
