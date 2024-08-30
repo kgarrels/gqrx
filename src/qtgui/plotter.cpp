3389545e5571ff3e7e26b5aea1e5ec798b3de890
kgarrels <kgarrels@users.noreply.github.com>
2024-08-30 12:10:46 +0200

    plotter, simplify autorange

--- src/qtgui/plotter.cpp
+++ src/qtgui/plotter.cpp
@@ -1906,26 +1906,19 @@ void CPlotter::setNewFftData(const float *fftData, int size)
 
         long offset = (long) size / 4;      // skip the 1st quarter and the last quarter of the spectrum
 
-        std::vector<float>fftCopy;
-        fftCopy.resize(size - 2*offset);
-        std::copy(&m_fftIIR[offset], &m_fftIIR[size-offset], &fftCopy[0]);      // copy from +offset to size-offet
-        std::sort(std::begin(fftCopy), std::begin(fftCopy)+size-2*offset);      // sort
+        std::vector<float>fftCopy(m_fftIIR.size());                                          // will be too big, who cares
+        std::copy(m_fftIIR.begin()+offset, m_fftIIR.end()-offset , fftCopy.begin());        // copy from +offset to size-offet
+        std::sort(std::begin(fftCopy), std::begin(fftCopy)+size-2*offset);                  // sort
         
         const long bins = 20; //=(size -2*offset)/10;
         lowestValue = std::accumulate(std::begin(fftCopy), std::begin(fftCopy)+bins, 0.0f) / bins;
-        //lowestValue = *std::min_element(std::begin(fftCopy), std::end(fftCopy));
-
+        
         // protect against NaN
         if (lowestValue != lowestValue) {
             qCDebug(plotter) << "lowestValue NaN";
             return;
         }
 
-        // we have a huge jump, reset all averages
-        if(std::max(minAvg, lowestValue) / std::min(minAvg, lowestValue) > 20 ) {
-            minAvg = lowestValue;
-            m_fftDataSize = 0;      // reset everything
-        }
         // do a moving averge
         const float alpha = 0.1f;
         minAvg = alpha*lowestValue + (1.0f-alpha)* minAvg;
