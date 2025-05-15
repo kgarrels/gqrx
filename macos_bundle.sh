22dfa1096dd96c64413c8e32e241f51440018120
kgarrels <kgarrels@users.noreply.github.com>
2025-05-15 18:11:10 +0200

    Update macos_bundle.sh
    Update macos_bundle.sh
    
    Update macos_bundle.sh

--- macos_bundle.sh
+++ macos_bundle.sh
@@ -2,12 +2,18 @@
 
 GQRX_VERSION="$(<build/version.txt)"
 IDENTITY=Y3GC27WZ4S
-BREW_PREFIX="$(brew --prefix)"
-MACDEPLOYQT6="${BREW_PREFIX}"/opt/qt@6/bin/macdeployqt
+#BREW_PREFIX="$(brew --prefix)"
 
+echo $CONDA_PREFIX
+#MACDEPLOYQT6="{$CONDA_PREFIX}"/bin/macdeployqt6
+MACDEPLOYQT6=/Users/runner/micromamba/envs/gqrx/bin/macdeployqt6
+
+echo $MACDEPLOYQT6
+micromamba info
+
+#rm -r Gqrx.app
 mkdir -p Gqrx.app/Contents/MacOS
 mkdir -p Gqrx.app/Contents/Resources
-mkdir -p Gqrx.app/Contents/soapy-modules
 
 /bin/cat <<EOM >Gqrx.app/Contents/Info.plist
 <?xml version="1.0" encoding="UTF-8"?>
@@ -43,28 +49,46 @@ EOM
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-  <key>com.apple.security.cs.allow-unsigned-executable-memory</key>
-  <true/>
+    <key>com.apple.security.cs.allow-unsigned-executable-memory</key>
+        <true/>
+    <key>com.apple.security.cs.allow-jit</key>
+        <true/>
+    <key>com.apple.security.cs.allow-unsigned-executable-memory</key>
+        <true/>
+    <key>com.apple.security.cs.disable-executable-page-protection</key>
+        <true/>
+    <key>com.apple.security.cs.disable-library-validation</key>
+        <true/>
+    <key>com.apple.security.cs.allow-dyld-environment-variables</key>
+        <true/>
 </dict>
 </plist>
 EOM
 
 cp build/src/gqrx Gqrx.app/Contents/MacOS
 cp resources/icons/gqrx.icns Gqrx.app/Contents/Resources
-# NOTE: PlutoSDR is built locally, so it will not in the brew path
-cp /usr/local/lib/SoapySDR/modules*/libPlutoSDRSupport.so Gqrx.app/Contents/soapy-modules
-cp "${BREW_PREFIX}"/lib/SoapySDR/modules*/libremoteSupport.so Gqrx.app/Contents/soapy-modules
-chmod 644 Gqrx.app/Contents/soapy-modules/*
+# NOTE: PlutoSDR is built locally, so it will not be in the brew path
+#cp "${CONDA_PREFIX}"/lib/SoapySDR/modules*/libremoteSupport.so Gqrx.app/Contents/soapy-modules
+#chmod 644 Gqrx.app/Contents/soapy-modules/*
 
-dylibbundler -s "${BREW_PREFIX}"/opt/icu4c/lib/ -od -b -x Gqrx.app/Contents/MacOS/gqrx -x Gqrx.app/Contents/soapy-modules/libPlutoSDRSupport.so -x Gqrx.app/Contents/soapy-modules/libremoteSupport.so -d Gqrx.app/Contents/libs/
-"${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite # TODO: Remove macdeployqt workaround
+#dylibbundler -s "${CONDA_PREFIX}"/lib -od -b -x Gqrx.app/Contents/MacOS/gqrx -x Gqrx.app/Contents/soapy-modules/libPlutoSDRSupport.so -x Gqrx.app/Contents/soapy-modules/libremoteSupport.so -d Gqrx.app/Contents/libs/
+#dylibbundler -s "${CONDA_PREFIX}"/lib -od -b -x Gqrx.app/Contents/MacOS/gqrx -x Gqrx.app/Contents/PlugIns/platforms/libqcocoa.dylib  -d Gqrx.app/Contents/libs/
+
+# echo '---macdeployqt---'
+
+# "${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite   # TODO: Remove macdeployqt workaround
+
+echo '---macdeployqt 2---'
+
 if [ "$1" = "true" ]; then
-    "${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite -sign-for-notarization="${IDENTITY}"
+    "${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite -sign-for-notarization="${IDENTITY}" -libpath=Gqrx.app/Contents/Frameworks
 else
-    "${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite
+    "${MACDEPLOYQT6}" Gqrx.app -no-strip -always-overwrite -libpath=Gqrx.app/Contents/Frameworks
 fi
 
-for f in Gqrx.app/Contents/libs/*.dylib Gqrx.app/Contents/soapy-modules/*.so Gqrx.app/Contents/Frameworks/*.framework Gqrx.app/Contents/Frameworks/*.dylib Gqrx.app/Contents/MacOS/gqrx
+echo '---codesign---'
+
+for f in Gqrx.app/Contents/libs/*.dylib Gqrx.app/Contents/Frameworks/*.dylib Gqrx.app/Contents/MacOS/gqrx
 do
     if [ "$1" = "true" ]; then
         codesign --force --verify --verbose --timestamp --options runtime --entitlements /tmp/Entitlements.plist --sign "${IDENTITY}" "$f"
